d907f8df7bfe6b6189a82d5f444fd18bb198fa14
Tom Duncalf <tom@tomduncalf.com>
2020-05-13 11:33:57 +0100

    Initial Commit

---
+++ SendMIDIBluetoothTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SendMIDIBluetoothTest
+//
+//  Created by Tom Duncalf on 13/05/2020.
+//  Copyright © 2020 td. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
